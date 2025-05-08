789be08ed873036ec772ed0331a2854f83b7017d
hazimmarhaimi <hazimmarhaimi94@gmail.com>
2025-05-08 16:33:38 +0800

    Add UserManagementController and UserManagementService

---
+++ FitnessAppAPI/Services/UserManagementService.cs
@@ -0,0 +1,32 @@
+﻿using FitnessAppAPI.Data;
+using FitnessAppAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FitnessAppAPI.Services
+{
+    public class UserManagementService
+    {
+        private readonly AppDbContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserManagementService(AppDbContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<List<User>> GetListUser()
+        {
+            var userList = await _context.Users.ToListAsync();
+            if (userList == null || !userList.Any())
+            {
+                throw new InvalidOperationException("No users found in the database");
+            }
+            return userList;
+        }
+    }
+}
